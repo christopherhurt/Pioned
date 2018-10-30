cd87fe627b49f6cd3be497392da539367f3c171a
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-10-30 16:06:32 -0400

    Move map into its own class ('GameMap')

---
+++ src/map.js
@@ -0,0 +1,19 @@
+export class GameMap {
+  constructor(cols, rows, tsize, dsize, layers) {
+    this.cols = cols;
+    this.rows = rows;
+    this.tsize = tsize;
+    this.dsize = dsize;
+    this.layers = layers;
+    this.width = cols * dsize;
+    this.height = rows * dsize;
+  }
+
+  getTile(layer, col, row) {
+    return this.layers[layer][row * this.cols + col];
+  }
+
+  setTile(layer, col, row, type) {
+    this.layers[layer][row * this.cols + col] = type;
+  }
+}
